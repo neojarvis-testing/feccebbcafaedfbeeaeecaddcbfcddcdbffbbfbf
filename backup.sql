df981b46be12e66c8f43c5065113498db49c9b0a
neojarvis <techadmin@iamneo.ai>
2024-02-15 17:40:36 +0000

    Manual-save-commit-15/02/2024--23:10:32

--- backup.sql
+++ backup.sql
@@ -22,6 +22,28 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `appdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `appdb`;
+
+--
+-- Table structure for table `vasanth`
+--
+
+DROP TABLE IF EXISTS `vasanth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vasanth` (
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vasanth`
+--
+
+LOCK TABLES `vasanth` WRITE;
+/*!40000 ALTER TABLE `vasanth` DISABLE KEYS */;
+INSERT INTO `vasanth` VALUES ('siva'),('ram');
+/*!40000 ALTER TABLE `vasanth` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -32,4 +54,4 @@ USE `appdb`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-15 17:38:21
+-- Dump completed on 2024-02-15 17:40:36
